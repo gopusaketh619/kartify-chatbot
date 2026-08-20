784f2596ceab7d8ffdee26af75abe7dda36c18cb
gopusaketh619 <30482384+gopusaketh619@users.noreply.github.com>
2026-08-20 10:51:29 -0700

    Update app.py

--- app.py
+++ app.py
@@ -26,8 +26,11 @@ st.set_page_config(
 file_name = 'config.json'
 with open(file_name, 'r') as file:
     config = json.load(file)
-    OPENAI_API_KEY = config.get("OPENAI_API_KEY") # Loading the API Key
-    OPENAI_API_BASE = config.get("OPENAI_API_BASE") # Loading the API Base Url
+    # OPENAI_API_KEY = config.get("OPENAI_API_KEY") # Loading the API Key
+    # OPENAI_API_BASE = config.get("OPENAI_API_BASE") # Loading the API Base Url
+    OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+    OPENAI_API_BASE = st.secrets["OPENAI_API_BASE"]
+
 
 
 # Storing API credentials in environment variables
